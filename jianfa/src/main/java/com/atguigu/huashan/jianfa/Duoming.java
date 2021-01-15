a1018a66bb6a6b81c8072a9f151273681b801451
myt111 <1229343081@qq.com>
2021-01-15 20:56:37 +0800

    增加孤独九剑

---
+++ jianfa/src/main/java/com/atguigu/huashan/jianfa/Duoming.java
@@ -0,0 +1,13 @@
+package com.atguigu.huashan.jianfa;
+
+/**
+ * @author mytstart
+ * @create 16:10-01-15
+ */
+public class Duoming {
+    public static void main(String[] args) {
+        System.out.println("孤独九剑");
+        System.out.println("孤独九剑");
+        System.out.println("孤独九剑");
+    }
+}
